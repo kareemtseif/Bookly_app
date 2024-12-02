ac6795893a6f52934682c0a17fecf9df8a3f2150
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-02 21:04:37 +0200

    Add HomeView and update SplashViewBody for navigation
    Created a new `HomeView` widget and updated `SplashViewBody` to include a sliding animation initialization and navigation to `HomeView` after a delay. Introduced a new constant `kTransitionDuration` for transition effects.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -15,13 +18,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-
-    animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 900));
-    slidingAnimation = Tween<Offset>(
-            begin: const Offset(0.0, 5.5), end: const Offset(0.0, 0.0))
-        .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
 
   @override
@@ -35,5 +33,24 @@ class _SplashViewBodyState extends State<SplashViewBody>
     return Center(
       child: SlidingImage(slidingAnimation: slidingAnimation),
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 900));
+    slidingAnimation = Tween<Offset>(
+            begin: const Offset(0.0, 5.5), end: const Offset(0.0, 0.0))
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      return Get.to(
+        () => const HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
