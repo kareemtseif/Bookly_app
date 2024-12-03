9d642b93d0f9942e6fc70a377b8624b871f2d761
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-03 20:39:36 +0200

    Update title font size and add Google Fonts
    Adjusted the font size for the titleMedium style to 18 and integrated Google Fonts to use Montserrat for the text theme in the app. Also, added necessary dependencies and imports for Google Fonts and Path Provider.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/colors.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -14,8 +15,12 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark()
-          .copyWith(scaffoldBackgroundColor: AppColors.kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+        ),
+      ),
       home: const SplashView(),
     );
   }
