71dbb5deff380d8704f6891061ba8c31388e9cf3
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-06 19:01:49 +0200

    Refactor BestSellerListViewItem and Update Styles
    Moved BestSellerListViewItem to its own file and updated the textStyle20 to conditionally use kGTSectraFine. Also, cleaned up the HomeViewBody by removing the duplicate BestSellerListViewItem widget.

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,74 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/images.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.15,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.4 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: const Color(0xffFEBDA6),
+                  image: const DecorationImage(
+                    image: AssetImage(
+                      AppImages.image,
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry Potter\nand the Goblet of Fire',
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/images.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -28,55 +30,6 @@ class HomeViewBody extends StatelessWidget {
           ),
           BestSellerListViewItem(),
         ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.15,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.4 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: const Color(0xffFEBDA6),
-                  image: const DecorationImage(
-                    image: AssetImage(
-                      AppImages.image,
-                    ),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: const Text(
-                    'Harry Potter\nand the Goblet of Fire',
-                    style: Styles.textStyle20,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
