3fbd3c5c4b0a2ebc6d1c3233b87b9f073cb9c447
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-08 20:24:15 +0200

    Add CustomBookImage widget and update usage in BookDetailsViewBody and FeaturedBooksListView
    Added a new `CustomBookImage` widget to display book images with a specific aspect ratio and styling. Updated `BookDetailsViewBody` to include this widget and adjusted padding. Also, replaced `FeaturedListViewItem` with `CustomBookImage` in `FeaturedBooksListView` to maintain consistency.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'custom_book_details_app_bar.dart';
 
@@ -6,12 +7,17 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
           children: [
-            CustomBookDetailsAppBar(),
+            const CustomBookDetailsAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.2),
+              child: const CustomBookImage(),
+            ),
           ],
         ),
       ),
