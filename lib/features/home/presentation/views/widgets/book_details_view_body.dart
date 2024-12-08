9ff3519e69a7718c6fc5379526c52081038a1489
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-08 20:46:02 +0200

    Add book title and author to BookDetailsViewBody
    Updated the BookDetailsViewBody to include the book title and author with appropriate styling and spacing. Adjusted the horizontal padding for the CustomBookImage widget for better alignment.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'custom_book_details_app_bar.dart';
@@ -15,8 +16,30 @@ class BookDetailsViewBody extends StatelessWidget {
             const CustomBookDetailsAppBar(),
             Padding(
               padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.2),
+                  horizontal: MediaQuery.of(context).size.width * 0.22),
               child: const CustomBookImage(),
+            ),
+            const SizedBox(
+              height: 43,
+            ),
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: 0.7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
           ],
         ),
