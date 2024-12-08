b32baf944d6b233262b88ec66a9e75622e8e8cd2
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-08 21:06:04 +0200

    Remove unused color and enhance book rating widget
    Removed the unused `kNumberColor` from `colors.dart` and improved the `BookRating` widget by adding a `mainAxisAlignment` parameter, setting icon size, and adjusting text styles and opacity.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'custom_book_details_app_bar.dart';
@@ -40,6 +41,12 @@ class BookDetailsViewBody extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                 ),
               ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            const BookRating(
+              mainAxisAlignment: MainAxisAlignment.center,
             ),
           ],
         ),
