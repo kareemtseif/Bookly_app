7a442df8efdef026bcb2d010c44ea6f532692840
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-19 00:33:28 +0200

    Update Android Gradle Plugin and Add CustomButton and BooksAction Widgets
    Updated the Android Gradle Plugin version to 8.1.4 in `settings.gradle`. Added a new `CustomButton` widget in `custom_button.dart` and a `BooksAction` widget in `books_action.dart`. Also, integrated the `BooksAction` widget into `book_details_view_body.dart`.

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,42 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.text,
+    this.borderRadius,
+    this.fontSize,
+  });
+  final Color backgroundColor;
+  final Color textColor;
+  final String text;
+  final BorderRadius? borderRadius;
+  final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: Styles.textStyle18.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -48,6 +49,8 @@ class BookDetailsViewBody extends StatelessWidget {
             const BookRating(
               mainAxisAlignment: MainAxisAlignment.center,
             ),
+            const SizedBox(height: 37),
+            const BooksAction(),
           ],
         ),
       ),
