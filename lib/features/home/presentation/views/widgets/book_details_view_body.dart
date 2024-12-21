e931694580a9c8f9cb2114d4b8723acb32fc8cc4
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-21 09:18:00 +0200

    Refactor BookDetailsViewBody to use CustomScrollView
    Updated the BookDetailsViewBody to use a CustomScrollView with a SliverFillRemaining to improve scroll behavior and layout structure. This change ensures that the content is properly managed within a scrollable area, enhancing the user experience.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -11,63 +11,72 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.22),
-              child: const CustomBookImage(),
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            Text(
-              'The Jungle Book',
-              style: Styles.textStyle30.copyWith(
-                fontWeight: FontWeight.w600,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  const CustomBookDetailsAppBar(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.22),
+                    child: const CustomBookImage(),
+                  ),
+                  const SizedBox(
+                    height: 43,
+                  ),
+                  Text(
+                    'The Jungle Book',
+                    style: Styles.textStyle30.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      'Rudyard Kipling',
+                      style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(height: 37),
+                  const BooksAction(),
+                  const Expanded(
+                    child: SizedBox(height: 50),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You might also like',
+                      style: Styles.textStyle16.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const SimilarBooksListView(),
+                  const SizedBox(height: 40),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 6,
-            ),
-            Opacity(
-              opacity: 0.7,
-              child: Text(
-                'Rudyard Kipling',
-                style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(height: 37),
-            const BooksAction(),
-            const SizedBox(height: 50),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You might also like',
-                style: Styles.textStyle16.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            const SimilarBooksListView(),
-            const SizedBox(height: 40),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
