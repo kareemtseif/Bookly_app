ec155675e274d88f5fe9f8229fea24bdd711f5a1
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-21 13:18:36 +0200

    Refactor BestSellerListView to use BookListViewItem and add SearchResultListView
    Updated BestSellerListView to use the new BookListViewItem widget. Also, added a new SearchResultListView widget and integrated it into the SearchViewBody.

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_search_text_field.dart';
+import 'search_result_list_view.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -9,8 +11,18 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(height: 16),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: SearchResultListView(),
+          ),
         ],
       ),
     );
