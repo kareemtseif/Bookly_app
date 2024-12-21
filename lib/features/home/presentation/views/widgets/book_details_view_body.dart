1ac001ecb69d0688379343ebe2cd206d590b322b
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-21 09:12:43 +0200

    Add SimilarBooksListView to BookDetailsViewBody
    Added a new widget `SimilarBooksListView` to display a list of similar books below the book actions in the `BookDetailsViewBody`. This includes updating the import statements and adding the necessary UI elements to the `BookDetailsViewBody` class.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_
 import 'package:flutter/material.dart';
 import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
+import 'similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -51,6 +52,19 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
             const SizedBox(height: 37),
             const BooksAction(),
+            const SizedBox(height: 50),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You might also like',
+                style: Styles.textStyle16.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const SimilarBooksListView(),
+            const SizedBox(height: 40),
           ],
         ),
       ),
