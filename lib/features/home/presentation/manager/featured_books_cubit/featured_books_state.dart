59057623587aea2d10c9b3e1467a72825be0761b
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-23 02:39:26 +0200

    Add NewestBooksCubit and update FeaturedBooksState class declarations
    Added a new `NewestBooksCubit` for managing the state of newest books. Also, updated the class declarations in `FeaturedBooksState` to use `class` instead of `final class` for consistency.

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -9,14 +9,14 @@ sealed class FeaturedBooksState extends Equatable {
 
 final class FeaturedBooksInitial extends FeaturedBooksState {}
 
-final class FeaturedBooksLoading extends FeaturedBooksState {}
+class FeaturedBooksLoading extends FeaturedBooksState {}
 
-final class FeaturedBooksFailure extends FeaturedBooksState {
+class FeaturedBooksFailure extends FeaturedBooksState {
   final String errMessage;
   const FeaturedBooksFailure(this.errMessage);
 }
 
-final class FeaturedBooksSuccess extends FeaturedBooksState {
+class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
   const FeaturedBooksSuccess(this.books);
 }
