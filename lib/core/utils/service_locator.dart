2e568c3559ad055ffbc7388fbf2bebd5dfd4761e
Kareem Tarek Seif <kareemtseif@gmail.com>
2024-12-24 12:26:39 +0200

    Add service locator setup and dependency injection
    Implemented a service locator using GetIt to manage dependencies such as ApiService and HomeRepoImp. Updated main.dart to initialize the service locator and use MultiBlocProvider for state management in the BooklyApp widget. Added GetIt as a dependency in pubspec.yaml and pubspec.lock.

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImp>(
+    HomeRepoImp(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
